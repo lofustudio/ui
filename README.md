0fd4a414d536ee65f8ce1ec675adeb1c87897b1e
Ty Mason <hi@tygr.dev>
2023-04-16 22:00:57 +0100

    Release 0.1.0-beta.1

---
+++ README.md
@@ -0,0 +1,28 @@
+# [@lofu/ui](https://npmjs.com/packages/@lofu/ui)
+
+> 🛠️ Our official React UI library, built with Radix UI and TailwindCSS.
+
+## Installation
+
+To use LofUI, all you need to do is install the `@lofu/ui` package and its peer dependencites:
+
+```sh
+# pnpm (recommended)
+pnpm add @lofu/ui react react-dom
+
+# yarn
+yarn add @lofu/ui react react-dom
+
+# npm
+npm install @lofu/ui react react-dom
+```
+
+Now you can start using components:
+
+```tsx
+import { Button } from "@lofu/ui";
+
+function Example() {
+    return <Button label="Button" />
+}
+```
---
+++ rollup.config.mjs
@@ -0,0 +1,41 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+import PeerDepsExternal from "rollup-plugin-peer-deps-external";
+
+import pkg from './package.json' assert { type: 'json' };
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: pkg.main,
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: pkg.module,
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            PeerDepsExternal(),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: "./tsconfig.json" }),
+            postcss(),
+            terser(),
+        ],
+    },
+    {
+        input: "dist/esm/types/index.d.ts",
+        output: [{ file: "dist/index.d.ts", format: "esm" }],
+        plugins: [dts()],
+        external: [/\.(css|less|scss)$/]
+    },
+];
